a1a251491f09af1d033124e16b52566d1ab88b5c
Nicco Vernasco <nivernas@gmail.com>
2023-07-31 13:20:44 -0400

    completed flash card project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Layout from "./Layout";
+import Layout from "./Layout/Layout";
 import "./App.css";
 
 /**
@@ -9,7 +9,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="app-routes">
+    <div className="app-routes app-background">
       <Switch>
         <Route path="/">
           <Layout />
--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
-    <header className="jumbotron bg-dark">
-      <div className="container text-white">
+    <header className="jumbotron bg-transparent">
+      <div className="container text-black">
         <h1 className="display-4">Flashcard-o-matic</h1>
-        <p className="lead">Discover The Flashcard Difference.</p>
+        <p className="lead font-italic ml-1">Discover the flashcard difference.</p>
       </div>
     </header>
   );
